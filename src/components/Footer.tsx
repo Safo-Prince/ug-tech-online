0670f494ea973c49f28f8bfcc15f369723094873
Safo Prince <psam64850@gmail.com>
2023-12-12 23:22:28 +0000

    added animation to the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Facebook, Instagram } from "iconsax-react";
 import { TwitterLogo, SnapchatLogo } from "@phosphor-icons/react";
+import { motion } from "framer-motion";
 
 const navigation = {
   about: [
@@ -33,8 +34,13 @@ const navigation = {
 const Footer: React.FC = () => {
   return (
     <footer className="bg-[#153D6D] ">
-      <div className="mx-auto max-w-7xl px-6 py-16 sm:py-20 lg:px-8 lg:py-24  flex sm:flex-row flex-col gap-y-5 sm:gap-y-0   sm:justify-between items-center">
-        <div>
+      <motion.div className="mx-auto max-w-7xl px-6 py-16 sm:py-20 lg:px-8 lg:py-24  flex sm:flex-row flex-col gap-y-5 sm:gap-y-0   sm:justify-between items-center">
+        <motion.div
+          initial={{ y: 100, opacity: 0.3 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.3, delay: 0 }}
+          viewport={{ once: false }}
+        >
           <h3 className="text-lg font-poppins font-bold leading-6 text-[#EAEAEB] text-center sm:text-left">
             About
           </h3>
@@ -53,8 +59,13 @@ const Footer: React.FC = () => {
               </li>
             ))}
           </ul>
-        </div>
-        <div className="">
+        </motion.div>
+        <motion.div
+          initial={{ y: 100, opacity: 0.3 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.3, delay: 0.1 }}
+          viewport={{ once: false }}
+        >
           <h3 className="text-lg font-poppins font-bold leading-6 text-[#EAEAEB] text-center  sm:text-left">
             Support
           </h3>
@@ -73,8 +84,13 @@ const Footer: React.FC = () => {
               </li>
             ))}
           </ul>
-        </div>
-        <div>
+        </motion.div>
+        <motion.div
+          initial={{ y: 100, opacity: 0.3 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.3, delay: 0.2 }}
+          viewport={{ once: false }}
+        >
           <h3 className="text-base font-poppins  font-bold leading-6 text-[#EAEAEB]  text-center sm:text-left">
             Quick Links
           </h3>
@@ -93,27 +109,33 @@ const Footer: React.FC = () => {
               </li>
             ))}
           </ul>
-        </div>
-        <div className="space-y-3 mt-2 sm:mt-0">
+        </motion.div>
+        <motion.div
+          initial={{ y: 100, opacity: 0.3 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.3, delay: 0.3 }}
+          viewport={{ once: false }}
+          className="space-y-3 mt-2 sm:mt-0"
+        >
           <div className="flex gap-3">
-            <div className=" h-12 w-12 sm:h-14 sm:w-14 cursor-pointer  border-2 border-[#CCCCCC] rounded-full flex items-center justify-center hover:text-white">
+            <div className=" h-12 w-12 sm:h-14 sm:w-14 cursor-pointer  border-2 border-[#CCCCCC] rounded-full flex items-center justify-center hover:text-white hover:-translate-y-2 transition-all">
               <Facebook size="25" color="#CCCCCC" variant="Bold" />
             </div>
-            <div className=" h-12 w-12 sm:h-14 sm:w-14 cursor-pointer border-2 border-[#CCCCCC] rounded-full flex items-center justify-center  text-white">
+            <div className=" h-12 w-12 sm:h-14 sm:w-14 cursor-pointer border-2 border-[#CCCCCC] rounded-full flex items-center justify-center  text-white hover:-translate-y-2 transition-all">
               <TwitterLogo size={25} color="#CCCCCC" />
             </div>
-            <div className=" h-12 w-12 sm:h-14 sm:w-14 cursor-pointer border-2 border-[#CCCCCC] rounded-full flex items-center justify-center  text-white">
+            <div className=" h-12 w-12 sm:h-14 sm:w-14 cursor-pointer border-2 border-[#CCCCCC] rounded-full flex items-center justify-center  text-white hover:-translate-y-2 transition-all">
               <SnapchatLogo size={25} color="#CCCCCC" />
             </div>
-            <div className=" h-12 w-12 sm:h-14 sm:w-14 cursor-pointer border-2 border-[#CCCCCC] rounded-full flex items-center justify-center  text-white">
+            <div className=" h-12 w-12 sm:h-14 sm:w-14 cursor-pointer border-2 border-[#CCCCCC] rounded-full flex items-center justify-center  text-white hover:-translate-y-2 transition-all">
               <Instagram color="#CCCCCC" />
             </div>
           </div>
-          <h1 className="text-[#EAEAEB] text-center sm:text-right tracking-normal font-normal font-lato text-base ">
+          <p className="text-[#EAEAEB] text-center sm:text-right tracking-normal font-normal font-lato text-base ">
             UG TechOnline &copy; 2021
-          </h1>
-        </div>
-      </div>
+          </p>
+        </motion.div>
+      </motion.div>
     </footer>
   );
 };
