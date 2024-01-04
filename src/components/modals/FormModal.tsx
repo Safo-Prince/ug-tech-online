3b26715929b2072ca83f0278117c54b7d0b5e684
Kcee <80968110+irondijonathan@users.noreply.github.com>
2024-01-04 02:07:09 +0000

    recreate dist

--- src/components/modals/FormModal.tsx
+++ src/components/modals/FormModal.tsx
@@ -411,7 +411,8 @@ const FormModal: React.FC<Props> = ({ open, setOpen }) => {
                           Hospitalities & Tourism
                         </option>
                         <option value="Information & Communication Technology including Software">
-                          Information & Communication Technology including Software
+                          Information & Communication Technology including
+                          Software
                         </option>
                         <option value="Health & Biotechnology">
                           Health & Biotechnology
